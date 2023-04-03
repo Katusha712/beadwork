b140e4337c08953193e5ec0e03b4238442dbe3f5
Kateryna Dubova <kateryna.dubova@nure.ua>
2023-04-03 14:35:53 +0300

    Заповнення класу Picture

--- infrastructure/BeadworkMemory/PictureRepository.cs
+++ infrastructure/BeadworkMemory/PictureRepository.cs
@@ -10,7 +10,8 @@ namespace Beadwork.Memory
             new Picture(1, "Крик"),
             new Picture(2, "Зоряна ніч"),
             new Picture(3, "Поцілунок"),
-
+            new Picture(4, "Дівчина з перловою сережкою"),
+            new Picture(5, "Пані з горностаєм"),
         };
 
         public Picture[] GetAllByTitle(string titlePart)
